52e8526871bf65e1caffe25af022c68b89367e71
Dan Sandland <dsandl20@gmail.com>
2017-03-12 20:27:35 -0700

    Initial

---
+++ general/spiders/general_spider.py
@@ -0,0 +1,22 @@
+from scrapy.spiders import Spider
+from scrapy.http import Request
+from scrapy.http.response.html import HtmlResponse
+from scrapy.linkextractors import LinkExtractor
+
+
+class JobSpider(Spider):
+    name = 'jobspider'
+
+    def __init__(self, *args, **kwargs):
+        super(GeneralSpider, self).__init__(*args, **kwargs)
+        self.le = LinkExtractor()
+
+    def parse(self, response):
+        if not isinstance(response, HtmlResponse):
+            return
+
+        for link in self.le.extract_links(response):
+            r = Request(url=link.url)
+            r.meta.update(link_text=link.text)
+            yield r
+
